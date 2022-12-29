e5e5441ba352e6110d227a2ae7769e794cf40a57
Jinyoung Jang <jyjang@uengine.org>
2022-12-29 03:48:48 +0000

    c

--- src/main/java/com/example/banking/BankingApplication.java
+++ src/main/java/com/example/banking/BankingApplication.java
@@ -1,13 +1,13 @@
-package com.demo.petstore;
+package com.example.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetstoreApplication {
+public class BankingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetstoreApplication.class, args);
+		SpringApplication.run(BankingApplication.class, args);
 	}
 
 }
